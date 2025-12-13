70a04e66dae4cfba835c2e42894a3eb1b904f4c7
fawmeen <mohamedfawmeen76@gmail.com>
2025-12-13 11:15:05 +0530

    navbar created as a first step

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+ import { navLink } from '../Constants'
+
+const Navbar = () => {
+  return (
+   <header>
+      <nav>
+         <img src="/logo.svg" alt = "Apple Logo" />
+         <ul>
+            {navLink.map(({label}) => (
+                <li key={label}>
+                    <a href={label}>{label}</a>
+                </li>
+            ))
+            }
+         </ul>
+
+         <div className='flex-center gap-3'>
+            <button>
+                <img src="/search.svg" alt = "Search" />
+            </button>
+            <button>
+                <img src="/cart.svg" alt = "cart" />
+            </button>
+         </div>
+      </nav>
+   </header>
+  )
+}
+
+export default Navbar
